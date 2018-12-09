800de2e5e584fd737563c6be32e77744b25313d5
PrzemyslawKlys <pklys@evotec.pl>
2018-12-09 12:01:19 +0100

    Update

--- Private/Remove-Directory.ps1
+++ Private/Remove-Directory.ps1
@@ -1,14 +1,15 @@
 function Remove-Directory {
     [CmdletBinding()]
     param (
-        $dir
+        [string] $Dir
     )
-
-    $exists = Test-Path $dir
-    if ($exists) {
-        #Write-Color 'Removing directory ', $dir -Color White, Yellow
-        Remove-Item $dir -Confirm:$false -Recurse
-    } else {
-        #Write-Color 'Removing directory ', $dir, ' skipped.' -Color White, Yellow, Red
+    if (-not [string]::IsNullOrWhiteSpace($Dir)) {
+        $exists = Test-Path -Path $Dir
+        if ($exists) {
+            #Write-Color 'Removing directory ', $dir -Color White, Yellow
+            Remove-Item $dir -Confirm:$false -Recurse
+        } else {
+            #Write-Color 'Removing directory ', $dir, ' skipped.' -Color White, Yellow, Red
+        }
     }
 }
