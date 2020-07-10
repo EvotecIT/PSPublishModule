1e24468513e5bd22ad1fe55d67b1111f0298a5d6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-11 00:09:32 +0200

    Update module

---
+++ Private/Step-Version.ps1
@@ -0,0 +1,21 @@
+﻿function Step-Version {
+    [cmdletBinding()]
+    param(
+        [string] $Module,
+        [ValidateSet('Major', 'Minor', 'Build', 'Revision')][string] $Update = 'Build'
+    )
+    $ModuleGallery = Find-Module -Name $Module
+    [version] $CurrentVersion = [version] $ModuleGallery.Version
+    $Types = @('Major', 'Minor', 'Build', 'Revision')
+    $NewVersion = foreach ($Type in $Types) {
+        if ($Type -eq $Update) {
+            $CurrentVersion.$Update + 1
+        } else {
+            if ($CurrentVersion.$Type -ne -1) {
+                $CurrentVersion.$Type
+            }
+        }
+    }
+    $NewVersion -join '.'
+}
+#Step-Version -Module Testimo -Update Minor
