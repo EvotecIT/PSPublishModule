45751f9fb2a17ae878eec051887534690669a141
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-14 12:45:32 +0200

    Fix for nested AST tokens

--- PSPublishModule.psd1
+++ PSPublishModule.psd1
@@ -7,13 +7,13 @@
     Description          = 'Simple project allowing preparing, managing and publishing modules to PowerShellGallery'
     FunctionsToExport    = 'Get-GitLog', 'Get-MissingFunctions', 'Initialize-PortableScript', 'New-PrepareModule', 'Register-Certificate', 'Remove-Comments', 'Test-ScriptFile', 'Test-ScriptModule'
     GUID                 = 'eb76426a-1992-40a5-82cd-6480f883ef4d'
-    ModuleVersion        = '0.9.18'
+    ModuleVersion        = '0.9.19'
     PowerShellVersion    = '5.1'
     PrivateData          = @{
         PSData = @{
             Tags                       = 'Windows', 'MacOS', 'Linux', 'Build', 'Module'
             ProjectUri                 = 'https://github.com/EvotecIT/PSPublishModule'
-            ExternalModuleDependencies = 'Microsoft.PowerShell.Utility', 'Microsoft.PowerShell.Archive', 'Microsoft.PowerShell.Management'
+            ExternalModuleDependencies = 'Microsoft.PowerShell.Utility', 'Microsoft.PowerShell.Archive', 'Microsoft.PowerShell.Management', 'Microsoft.PowerShell.Security'
             IconUri                    = 'https://evotec.xyz/wp-content/uploads/2019/02/PSPublishModule.png'
         }
     }
@@ -29,6 +29,6 @@
         ModuleVersion = '1.19.0'
         ModuleName    = 'PSScriptAnalyzer'
         Guid          = 'd6245802-193d-4068-a631-8863a4342a18'
-    }, 'Microsoft.PowerShell.Utility', 'Microsoft.PowerShell.Archive', 'Microsoft.PowerShell.Management'
+    }, 'Microsoft.PowerShell.Utility', 'Microsoft.PowerShell.Archive', 'Microsoft.PowerShell.Management', 'Microsoft.PowerShell.Security'
     RootModule           = 'PSPublishModule.psm1'
 }
--- Private/Get-ScriptCommands.ps1
+++ Private/Get-ScriptCommands.ps1
@@ -1,23 +1,20 @@
-﻿function Get-ScriptCommandsOld {
+﻿function Get-ScriptCommands {
     [CmdletBinding()]
     param(
         [string] $FilePath,
+        [alias('ScriptBlock')][scriptblock] $Code,
         [switch] $CommandsOnly
     )
     $astTokens = $null
     $astErr = $null
 
-    $null = [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$astTokens, [ref]$astErr)
-
-    $Commands = [System.Collections.Generic.List[Object]]::new()
-
-    foreach ($_ in $astTokens) {
-        if ($_.TokenFlags -eq 'Command' -and $_.Kind -eq 'Generic') {
-            if ($_.Value -notin $Commands) {
-                $Commands.Add($_)
-            }
-        }
+    if ($FilePath) {
+        $null = [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$astTokens, [ref]$astErr)
+    } else {
+        $null = [System.Management.Automation.Language.Parser]::ParseInput($Code, [ref]$astTokens, [ref]$astErr)
     }
+    $Commands = [System.Collections.Generic.List[Object]]::new()
+    Get-AstTokens -ASTTokens $astTokens -Commands $Commands
     if ($CommandsOnly) {
         $Commands.Value | Sort-Object -Unique
     } else {
@@ -27,7 +24,26 @@
     #$Commands = $astTokens | Where-Object { $_.TokenFlags -eq 'Command' } | Sort-Object -Property Value -Unique
 }
 
-function Get-ScriptCommands {
+function Get-AstTokens {
+    [cmdletBinding()]
+    param(
+        [System.Management.Automation.Language.Token[]] $ASTTokens,
+        [System.Collections.Generic.List[Object]] $Commands
+    )
+    foreach ($_ in $astTokens) {
+        if ($_.TokenFlags -eq 'Command' -and $_.Kind -eq 'Generic') {
+            if ($_.Value -notin $Commands) {
+                $Commands.Add($_)
+            }
+        } else {
+            if ($_.NestedTokens) {
+                Get-AstTokens -ASTTokens $_.NestedTokens -Commands $Commands
+            }
+        }
+    }
+}
+
+function Get-ScriptCommandsUseless {
     [cmdletBinding(DefaultParameterSetName = 'File')]
     param (
         [alias('Path')][Parameter(ParameterSetName = 'File')][string] $FilePath,
@@ -51,11 +67,15 @@ function Get-ScriptCommands {
             }
         }
         if ($CodeRead) {
+            #$Types = [System.Collections.Generic.List[string]]::new()
             $Tokens = [System.Management.Automation.PSParser]::Tokenize($CodeRead, [ref]$Errors)
             $Commands = foreach ($_ in $Tokens) {
                 if ($_.Type -eq 'Command') {
                     $_
                 }
+                #else {
+                #  $Types.Add($_.Type)
+                #}
             }
             if ($CommandsOnly) {
                 $Commands.Content | Sort-Object -Unique
