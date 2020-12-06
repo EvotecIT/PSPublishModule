5982e825215fc4cbd3f1052f632cf1d03c704c8a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-06 17:39:55 +0100

    update

---
+++ Private/Remove-ItemAlternative.ps1
@@ -0,0 +1,49 @@
+﻿function Remove-ItemAlternative {
+    <#
+    .SYNOPSIS
+    Removes all files and folders within given path
+
+    .DESCRIPTION
+    Removes all files and folders within given path. Workaround for Access to the cloud file is denied issue
+
+    .PARAMETER Path
+    Path to file/folder
+
+    .PARAMETER SkipFolder
+    Do not delete top level folder
+
+    .EXAMPLE
+    Remove-ItemAlternative -Path "C:\Support\GitHub\GpoZaurr\Docs"
+
+    .EXAMPLE
+    Remove-ItemAlternative -Path "C:\Support\GitHub\GpoZaurr\Docs"
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('LiteralPath')][string] $Path,
+        [switch] $SkipFolder
+    )
+    if ($Path -and (Test-Path -LiteralPath $Path)) {
+        $Items = Get-ChildItem -LiteralPath $Path -Recurse
+        foreach ($Item in $Items) {
+            try {
+                $Item.Delete()
+            } catch {
+                Write-Warning "Remove-ItemAlternative - Couldn't delete $($Item.FullName), error: $($_.Exception.Message)"
+            }
+        }
+        if (-not $SkipFolder) {
+            $Item = Get-Item -LiteralPath $Path
+            try {
+                $Item.Delete($true)
+            } catch {
+                Write-Warning "Remove-ItemAlternative - Couldn't delete $($Item.FullName), error: $($_.Exception.Message)"
+            }
+        }
+    } else {
+        Write-Warning "Remove-ItemAlternative - Path $Path doesn't exists. Skipping. "
+    }
+}
--- Private/Start-ModuleBuilding.ps1
+++ Private/Start-ModuleBuilding.ps1
@@ -510,41 +510,70 @@
         }
     }
     if ($Configuration.Steps.BuildDocumentation) {
-        $WarningVariablesMarkdown = @()
-        $DocumentationPath = "$FullProjectPath\$($Configuration.Options.Documentation.Path)"
-        $ReadMePath = "$FullProjectPath\$($Configuration.Options.Documentation.PathReadme)"
-        Write-Text "[+] Generating documentation to $DocumentationPath with $ReadMePath" -Color Yellow
+        # Support for old way of building documentation -> converts to new one
+        if ($Configuration.Steps.BuildDocumentation -is [bool]) {
+            $TemporaryBuildDocumentation = $Configuration.Steps.BuildDocumentation
+            $Configuration.Steps.BuildDocumentation = @{
+                Enable = $TemporaryBuildDocumentation
+            }
+        }
+        # Real documentation process
+        if ($Configuration.Steps.BuildDocumentation -is [System.Collections.IDictionary]) {
+            if ($Configuration.Steps.BuildDocumentation.Enable -eq $true) {
+                $WarningVariablesMarkdown = @()
+                $DocumentationPath = "$FullProjectPath\$($Configuration.Options.Documentation.Path)"
+                $ReadMePath = "$FullProjectPath\$($Configuration.Options.Documentation.PathReadme)"
+                Write-Text "[+] Generating documentation to $DocumentationPath with $ReadMePath" -Color Yellow
 
-        if (-not (Test-Path -Path $DocumentationPath)) {
-            $null = New-Item -Path "$FullProjectPath\Docs" -ItemType Directory -Force
-        }
-        [Array] $Files = Get-ChildItem -Path $DocumentationPath
-        if ($Files.Count -gt 0) {
-            try {
-                $null = Update-MarkdownHelpModule $DocumentationPath -RefreshModulePage -ModulePagePath $ReadMePath -ErrorAction Stop -WarningVariable +WarningVariablesMarkdown -WarningAction SilentlyContinue -ExcludeDontShow
-            } catch {
-                Write-Text "[-] Documentation warning: $($_.Exception.Message)" -Color Yellow
-            }
-        } else {
-            try {
-                $null = New-MarkdownHelp -Module $ProjectName -WithModulePage -OutputFolder $DocumentationPath -ErrorAction Stop -WarningVariable +WarningVariablesMarkdown -WarningAction SilentlyContinue -ExcludeDontShow
-            } catch {
-                Write-Text "[-] Documentation warning: $($_.Exception.Message)" -Color Yellow
-            }
-            $null = Move-Item -Path "$DocumentationPath\$ProjectName.md" -Destination $ReadMePath
-            #Start-Sleep -Seconds 1
-            # this is temporary workaround - due to diff output on update
-            if ($Configuration.Options.Documentation.UpdateWhenNew) {
-                try {
-                    $null = Update-MarkdownHelpModule $DocumentationPath -RefreshModulePage -ModulePagePath $ReadMePath -ErrorAction Stop -WarningVariable +WarningVariablesMarkdown -WarningAction SilentlyContinue -ExcludeDontShow
-                } catch {
-                    Write-Text "[-] Documentation warning: $($_.Exception.Message)" -Color Yellow
+                if (-not (Test-Path -Path $DocumentationPath)) {
+                    $null = New-Item -Path "$FullProjectPath\Docs" -ItemType Directory -Force
                 }
+                [Array] $Files = Get-ChildItem -Path $DocumentationPath
+                if ($Files.Count -gt 0) {
+                    if ($Configuration.Steps.BuildDocumentation.StartClean -ne $true) {
+                        try {
+                            $null = Update-MarkdownHelpModule $DocumentationPath -RefreshModulePage -ModulePagePath $ReadMePath -ErrorAction Stop -WarningVariable +WarningVariablesMarkdown -WarningAction SilentlyContinue -ExcludeDontShow
+                        } catch {
+                            Write-Text "[-] Documentation warning: $($_.Exception.Message)" -Color Yellow
+                        }
+                    } else {
+                        # remove everything / Refresh Count
+                        #$null = Remove-Item -Path $DocumentationPath -Force -Recurse
+                        <#
+                        $ItemToDelete = Get-ChildItem -Path $DocumentationPath
+                        foreach ($Item in $ItemToDelete) {
+                            $Item.Delete()
+                        }
+                        $ItemToDelete = Get-Item -Path $DocumentationPath
+                        $ItemToDelete.Delete($true)
+                        #>
+                        Remove-ItemAlternative -Path $DocumentationPath -SkipFolder
+                        #$null = New-Item -Path "$FullProjectPath\Docs" -ItemType Directory -Force
+                        [Array] $Files = Get-ChildItem -Path $DocumentationPath
+                    }
+                }
+                if ($Files.Count -eq 0) {
+                    try {
+                        $null = New-MarkdownHelp -Module $ProjectName -WithModulePage -OutputFolder $DocumentationPath -ErrorAction Stop -WarningVariable +WarningVariablesMarkdown -WarningAction SilentlyContinue -ExcludeDontShow
+                    } catch {
+                        Write-Text "[-] Documentation warning: $($_.Exception.Message)" -Color Yellow
+                    }
+                    $null = Move-Item -Path "$DocumentationPath\$ProjectName.md" -Destination $ReadMePath
+                    #Start-Sleep -Seconds 1
+                    # this is temporary workaround - due to diff output on update
+                    if ($Configuration.Options.Documentation.UpdateWhenNew) {
+                        try {
+                            $null = Update-MarkdownHelpModule $DocumentationPath -RefreshModulePage -ModulePagePath $ReadMePath -ErrorAction Stop -WarningVariable +WarningVariablesMarkdown -WarningAction SilentlyContinue -ExcludeDontShow
+                        } catch {
+                            Write-Text "[-] Documentation warning: $($_.Exception.Message)" -Color Yellow
+                        }
+                    }
+                }
+                foreach ($_ in $WarningVariablesMarkdown) {
+                    Write-Text "[-] Documentation warning: $_" -Color Yellow
+                }
+
             }
-            #
-        }
-        foreach ($_ in $WarningVariablesMarkdown) {
-            Write-Text "[-] Documentation warning: $_" -Color Yellow
         }
     }
 
