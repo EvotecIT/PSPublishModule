5229764cf1f8d3f583f56c301ce95a76ec7f9d91
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-27 11:40:16 +0100

    Added cmdletbinding

--- Private/Set-SecurityProtocolForThread.ps1
+++ Private/Set-SecurityProtocolForThread.ps1
@@ -1,3 +1,5 @@
 ﻿function Set-SecurityProtocolForThread {
+    [CmdletBinding()]
+    param()
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12 -bor [System.Net.SecurityProtocolType]::Tls11 -bor [System.Net.SecurityProtocolType]::Tls
 }
