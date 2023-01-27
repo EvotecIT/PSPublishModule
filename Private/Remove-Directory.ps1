0c90987102980a07d4d2e55275b6c7d4f7ad5f68
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-27 20:51:32 +0100

    Cleanup function

--- Private/Remove-Directory.ps1
+++ Private/Remove-Directory.ps1
@@ -4,18 +4,15 @@ function Remove-Directory {
         [string] $Directory
     )
     if ($Directory) {
-        $exists = Test-Path -Path $Directory
-        if ($exists) {
-            #Write-Color 'Removing directory ', $dir -Color White, Yellow
+        $Exists = Test-Path -LiteralPath $Directory
+        if ($Exists) {
             try {
                 Remove-Item -Path $Directory -Confirm:$false -Recurse -Force -ErrorAction Stop
             } catch {
                 $ErrorMessage = $_.Exception.Message
                 Write-Text "[-] Can't delete folder $Directory. Fix error before contiuing: $ErrorMessage" -Color Red
-                Exit
+                return $false
             }
-        } else {
-            #Write-Color 'Removing directory ', $dir, ' skipped.' -Color White, Yellow, Red
         }
     }
 }
