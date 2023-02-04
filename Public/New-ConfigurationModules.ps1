77e3f5a970141f952b9719322f7c92839b6cd7a7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-04 18:12:13 +0100

    Required modules setup

---
+++ Public/New-ConfigurationModules.ps1
@@ -0,0 +1,27 @@
+﻿function New-ConfigurationModules {
+    [CmdletBinding()]
+    param(
+        [validateset('RequiredModule', 'ExternalModule')] $Type = 'RequiredModule',
+        [string] $Name,
+        [string] $Version,
+        [string] $Guid
+    )
+    $ModuleInformation = [ordered] @{
+        ModuleName    = $Name
+        ModuleVersion = $Version
+        Guid          = $Guid
+    }
+    Remove-EmptyValue -Hashtable $ModuleInformation
+    if ($ModuleInformation.Count -eq 0) {
+        return
+    } elseif ($ModuleInformation.Count -eq 1 -and $ModuleInformation.Contains('ModuleName')) {
+        $Configuration = $Name
+    } else {
+        $Configuration = $ModuleInformation
+    }
+    $Option = @{
+        Type          = $Type
+        Configuration = $Configuration
+    }
+    $Option
+}
