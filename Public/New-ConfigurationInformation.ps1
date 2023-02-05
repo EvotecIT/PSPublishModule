30dc1427887cc939c8b2c7f5d6a8d94e4c920852
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-05 21:59:05 +0100

    function to allow easy overwrite of some settings

---
+++ Public/New-ConfigurationInformation.ps1
@@ -0,0 +1,37 @@
+﻿function New-ConfigurationInformation {
+    [cmdletbinding()]
+    param(
+        [string] $FunctionsToExportFolder,
+        [string] $AliasesToExportFolder,
+        [string[]] $ExcludeFromPackage,
+        [string[]] $IncludeRoot,
+        [string[]] $IncludePS1,
+        [string[]] $IncludeAll,
+        [scriptblock] $IncludeCustomCode,
+        [System.Collections.IDictionary] $IncludeToArray,
+        [string] $LibrariesCore,
+        [string] $LibrariesDefault,
+        [string] $LibrariesStandard
+    )
+
+    $Configuration = [ordered] @{
+        FunctionsToExportFolder = $FunctionsToExportFolder
+        AliasesToExportFolder   = $AliasesToExportFolder
+        ExcludeFromPackage      = $ExcludeFromPackage
+        IncludeRoot             = $IncludeRoot
+        IncludePS1              = $IncludePS1
+        IncludeAll              = $IncludeAll
+        IncludeCustomCode       = $IncludeCustomCode
+        IncludeToArray          = $IncludeToArray
+        LibrariesCore           = $LibrariesCore
+        LibrariesDefault        = $LibrariesDefault
+        LibrariesStandard       = $LibrariesStandard
+    }
+    Remove-EmptyValue -Hashtable $Configuration
+
+    $Option = @{
+        Type          = 'Information'
+        Configuration = $Configuration
+    }
+    $Option
+}
