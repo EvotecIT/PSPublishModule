0204113bb09998370085b0c92aada5c57ee6d2f9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-16 09:24:26 +0100

    Add inital functions starter

---
+++ Public/New-ConfigurationArtefacts.ps1
@@ -0,0 +1,6 @@
+﻿function New-ConfigurationArtefacts {
+    [CmdletBinding()]
+    param(
+
+    )
+}
---
+++ Public/New-ConfigurationBuild.ps1
@@ -0,0 +1,6 @@
+﻿function New-ConfigurationBuild {
+    [CmdletBinding()]
+    param(
+
+    )
+}
