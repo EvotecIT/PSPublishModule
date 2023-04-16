a5d0a1c663e824852618674465ba53b69bb5f3d3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-16 19:55:56 +0200

    Configuration for publishing

--- Public/New-ConfigurationPublish.ps1
+++ Public/New-ConfigurationPublish.ps1
@@ -1,6 +1,46 @@
 ﻿function New-ConfigurationPublish {
     [CmdletBinding()]
     param(
+        [ValidateSet('PowerShellGallery', 'GitHub')]
+        [string] $Type,
 
+        [string] $FilePath,
+
+        [string] $UserName,
+
+        [string] $RepositoryName,
+
+        [string] $ApiKey
     )
+    $Options = [ordered] @{}
+    if ($Type -eq 'PowerShellGallery') {
+        $Options['PowerShellGallery'] = [ordered] @{}
+        if ($ApiKey) {
+            $Options.PowerShellGallery.ApiKey = $ApiKey
+            $Options.PowerShellGallery.FromFile = $false
+        } else {
+            $Options.PowerShellGallery.ApiKey = $FilePath
+            $Options.PowerShellGallery.FromFile = $true
+        }
+    } elseif ($Type -eq 'GitHub') {
+        $Options['GitHub'] = [ordered] @{}
+        if ($ApiKey) {
+            $Options.GitHub.ApiKey = $ApiKey
+            $Options.GitHub.FromFile = $false
+        } else {
+            $Options.GitHub.ApiKey = $FilePath
+            $Options.GitHub.FromFile = $true
+        }
+        if (-not $UserName) {
+            throw 'UserName is required for GitHub. Please fix New-ConfigurationPublish'
+        }
+        $Options.GitHub.UserName = $UserName
+        if ($RepositoryName) {
+            $Options.GitHub.RepositoryName = $RepositoryName
+        }
+    }
+    [ordered] @{
+        Type          = $Type
+        Configuration = $Options
+    }
 }
