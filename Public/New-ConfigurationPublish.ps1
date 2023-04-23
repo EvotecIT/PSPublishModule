c4fcc28717b548d1d9381b4c4a3d2834a9338d7e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-23 22:06:20 +0200

    Configuration for publishing

--- Public/New-ConfigurationPublish.ps1
+++ Public/New-ConfigurationPublish.ps1
@@ -10,37 +10,96 @@
 
         [string] $RepositoryName,
 
-        [string] $ApiKey
+        [string] $ApiKey,
+
+        [switch] $Enabled,
+
+        [string] $PreReleaseTag,
+
+        [switch] $Force
     )
     $Options = [ordered] @{}
+    $Skip = $false
     if ($Type -eq 'PowerShellGallery') {
         $Options['PowerShellGallery'] = [ordered] @{}
         if ($ApiKey) {
             $Options.PowerShellGallery.ApiKey = $ApiKey
             $Options.PowerShellGallery.FromFile = $false
-        } else {
+        } elseif ($FilePath) {
             $Options.PowerShellGallery.ApiKey = $FilePath
             $Options.PowerShellGallery.FromFile = $true
+        } else {
+            $Skip = $true
+        }
+        if (-not $Skip) {
+            [ordered] @{
+                Type          = $Type
+                Configuration = $Options
+            }
         }
     } elseif ($Type -eq 'GitHub') {
         $Options['GitHub'] = [ordered] @{}
         if ($ApiKey) {
             $Options.GitHub.ApiKey = $ApiKey
             $Options.GitHub.FromFile = $false
-        } else {
+        } elseif ($FilePath) {
             $Options.GitHub.ApiKey = $FilePath
             $Options.GitHub.FromFile = $true
+        } else {
+            $Skip = $true
         }
-        if (-not $UserName) {
-            throw 'UserName is required for GitHub. Please fix New-ConfigurationPublish'
-        }
-        $Options.GitHub.UserName = $UserName
-        if ($RepositoryName) {
-            $Options.GitHub.RepositoryName = $RepositoryName
+        # if user did try to set API KEY we would expect other stuff to be set
+        # otherwise lets skip it because maybe user wanted something else
+        if (-not $Skip) {
+            if (-not $UserName) {
+                throw 'UserName is required for GitHub. Please fix New-ConfigurationPublish'
+            }
+            $Options.GitHub.UserName = $UserName
+            if ($RepositoryName) {
+                $Options.GitHub.RepositoryName = $RepositoryName
+            }
+            [ordered] @{
+                Type          = $Type
+                Configuration = $Options
+            }
         }
     }
-    [ordered] @{
-        Type          = $Type
-        Configuration = $Options
+
+
+
+    if ($Type -eq 'PowerShellGallery') {
+        if ($Enabled) {
+            [ordered] @{
+                Type          = 'PowerShellGalleryPublishing'
+                PublishModule = [ordered] @{
+                    Enabled = $true
+                }
+            }
+        }
+        if ($PreReleaseTag) {
+            [ordered] @{
+                Type          = 'PowerShellGalleryPublishing'
+                PublishModule = [ordered] @{
+                    PreRelease = $PreReleaseTag
+                }
+            }
+        }
+        if ($Force) {
+            [ordered] @{
+                Type          = 'PowerShellGalleryPublishing'
+                PublishModule = [ordered] @{
+                    RequireForce = $Force.IsPresent
+                }
+            }
+        }
+    } else {
+        if ($Enabled) {
+            [ordered] @{
+                Type          = 'GitHubPublishing'
+                PublishModule = [ordered] @{
+                    GitHub = $true
+                }
+            }
+        }
     }
 }
