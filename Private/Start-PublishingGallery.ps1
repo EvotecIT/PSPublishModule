ffb41be2c17d8ad7daddb283503c664850952a71
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-30 19:34:47 +0200

    rename, plus optimization

---
+++ Private/Start-PublishingGallery.ps1
@@ -0,0 +1,22 @@
+﻿function Start-PublishingGallery {
+    [CmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $Configuration
+    )
+    Write-TextWithTime -Text "Publishing Module to PowerShellGallery" {
+        if ($Configuration.Options.PowerShellGallery.FromFile) {
+            $ApiKey = Get-Content -Path $Configuration.Options.PowerShellGallery.ApiKey -ErrorAction Stop -Encoding UTF8
+        } else {
+            $ApiKey = $Configuration.Options.PowerShellGallery.ApiKey
+        }
+        $publishModuleSplat = @{
+            Name        = $Configuration.Information.ModuleName
+            Repository  = 'PSGallery'
+            NuGetApiKey = $ApiKey
+            Force       = $Configuration.Steps.PublishModule.RequireForce
+            Verbose     = $Configuration.Steps.PublishModule.PSGalleryVerbose
+            ErrorAction = 'Stop'
+        }
+        Publish-Module @publishModuleSplat
+    } -PreAppend Plus
+}
