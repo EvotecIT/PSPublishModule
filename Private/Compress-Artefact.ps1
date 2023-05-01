023ae839cad9a06a5fa03af430012a9f6a0d9536
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-01 09:15:31 +0200

    move code to separate function

---
+++ Private/Compress-Artefact.ps1
@@ -0,0 +1,20 @@
+﻿function Compress-Artefact {
+    [CmdletBinding()]
+    param(
+        [string] $Destination,
+        [string] $FileName
+    )
+
+    $ZipPath = [System.IO.Path]::Combine($Destination, $FileName)
+
+    Write-TextWithTime -Text "Compressing final merged release $ZipPath" {
+        $null = New-Item -ItemType Directory -Path $Destination -Force
+        if ($DestinationPaths.Desktop) {
+            $CompressPath = [System.IO.Path]::Combine($DestinationPaths.Desktop, '*')
+            Compress-Archive -Path $CompressPath -DestinationPath $ZipPath -Force
+        } elseif ($DestinationPaths.Core) {
+            $CompressPath = [System.IO.Path]::Combine($DestinationPaths.Core, '*')
+            Compress-Archive -Path $CompressPath -DestinationPath $ZipPath -Force
+        }
+    } -PreAppend 'Plus'
+}
