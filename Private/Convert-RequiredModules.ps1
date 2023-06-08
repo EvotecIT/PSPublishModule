d1aa6af3e72a56c8e33fa2111f3a5b2a1c349410
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-08 11:03:26 +0200

    Fix for string based required modules

--- Private/Convert-RequiredModules.ps1
+++ Private/Convert-RequiredModules.ps1
@@ -24,22 +24,25 @@
 
     if ($Manifest.Contains('RequiredModules')) {
         foreach ($SubModule in $Manifest.RequiredModules) {
-            [Array] $AvailableModule = Get-Module -ListAvailable $SubModule.ModuleName -Verbose:$false
-
-            if ($SubModule.ModuleVersion -eq 'Latest') {
-                if ($AvailableModule) {
-                    $SubModule.ModuleVersion = $AvailableModule[0].Version.ToString()
-                } else {
-                    Write-Text -Text "[-] Module $($SubModule.ModuleName) is not available, but defined as required with last version. Terminating." -Color Red
-                    return $false
+            if ($SubModule -is [string]) {
+                #[Array] $AvailableModule = Get-Module -ListAvailable $SubModule -Verbose:$false
+            } else {
+                [Array] $AvailableModule = Get-Module -ListAvailable $SubModule.ModuleName -Verbose:$false
+                if ($SubModule.ModuleVersion -in 'Latest', 'Auto') {
+                    if ($AvailableModule) {
+                        $SubModule.ModuleVersion = $AvailableModule[0].Version.ToString()
+                    } else {
+                        Write-Text -Text "[-] Module $($SubModule.ModuleName) is not available, but defined as required with last version. Terminating." -Color Red
+                        return $false
+                    }
                 }
-            }
-            if ($SubModule.Guid -eq 'Auto') {
-                if ($AvailableModule) {
-                    $SubModule.Guid = $AvailableModule[0].Guid.ToString()
-                } else {
-                    Write-Text -Text "[-] Module $($SubModule.ModuleName) is not available, but defined as required with last version. Terminating." -Color Red
-                    return $false
+                if ($SubModule.Guid -in 'Latest', 'Auto') {
+                    if ($AvailableModule) {
+                        $SubModule.Guid = $AvailableModule[0].Guid.ToString()
+                    } else {
+                        Write-Text -Text "[-] Module $($SubModule.ModuleName) is not available, but defined as required with last version. Terminating." -Color Red
+                        return $false
+                    }
                 }
             }
         }
