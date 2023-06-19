4c7a9b0b6c281663029b2f2c96688958b200e453
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-19 10:51:40 +0200

    Easier new module creation

---
+++ Private/Register-DataForInitialModule.ps1
@@ -0,0 +1,13 @@
+﻿function Register-DataForInitialModule {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $FilePath,
+        [Parameter(Mandatory)][string] $ModuleName,
+        [Parameter(Mandatory)][string] $Guid
+    )
+
+    $BuildModule = Get-Content -Path $FilePath -Raw
+    $BuildModule = $BuildModule.Replace('$GUID', $Guid)
+    $BuildModule = $BuildModule.Replace('$ModuleName', $ModuleName)
+    Set-Content -Path $FilePath -Value $BuildModule -Encoding utf8
+}
