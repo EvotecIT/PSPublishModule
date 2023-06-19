66fe8eeeb66ee99ac2a5357bcf0043d7f4c67799
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-19 21:18:32 +0200

    Add basic examples

---
+++ Examples/Step01.CreateModuleProject.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module "$PSSCriptRoot\..\PSPublishModule.psd1" -Force
+
+# Step 01 - An initial module structure, manifest, and configuration file will be created
+Build-Module -ModuleName 'MyGreatModule' -Path "C:\Support\GitHub"
