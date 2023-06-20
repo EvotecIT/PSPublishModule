7da5e8952631908fccc38f24c25be00734749038
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-20 08:04:14 +0200

    Configuration for tests

---
+++ Public/New-ConfigurationTest.ps1
@@ -0,0 +1,24 @@
+﻿function New-ConfigurationTest {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][ValidateSet('BeforeMerge', 'AfterMerge')][string[]] $When,
+        [Parameter(Mandatory)][string] $TestsPath,
+        [switch] $Enable,
+        [switch] $Force
+    )
+
+    if ($Enable) {
+        foreach ($W in $When) {
+            $Configuration = [ordered] @{
+                Type          = "Tests$W"
+                Configuration = [ordered] @{
+                    When      = $W
+                    TestsPath = $TestsPath
+                    Force     = $Force.ispresent
+                }
+            }
+            Remove-EmptyValue -Hashtable $Configuration.Configuration
+            $Configuration
+        }
+    }
+}
