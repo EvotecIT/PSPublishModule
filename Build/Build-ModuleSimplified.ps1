c53b372c1b3067c7efcc7cdf31d93f7298203723
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-23 22:04:08 +0200

    Module builder example

--- Build/Build-ModuleSimplified.ps1
+++ Build/Build-ModuleSimplified.ps1
@@ -90,6 +90,6 @@ Build-Module -ModuleName 'PSPublishModule' {
     New-ConfigurationTest -TestsPath "$PSScriptRoot\..\Tests" -Enable
 
     # global options for publishing to github/psgallery
-    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$false
-    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$false
+    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
 }
