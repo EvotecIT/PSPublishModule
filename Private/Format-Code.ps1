da82dabe12b010a5099e8bcea2ec64fbc8f00ccb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-22 14:14:13 +0200

    Small improvements

--- Private/Format-Code.ps1
+++ Private/Format-Code.ps1
@@ -18,30 +18,30 @@
                 Write-Text "[i] Removing Comments - Lines in code after: 0" -Color Red
             }
         } else {
-            $Output = Write-TextWithTime -Text "[+] Reading file content - $FilePath" {
+            $Output = Write-TextWithTime -Text "Reading file content - $FilePath" {
                 Get-Content -LiteralPath $FilePath -Raw -Encoding UTF8
-            }
+            } -PreAppend Plus -SpacesBefore '   '
         }
         if ($null -eq $FormatCode.FormatterSettings) {
             $FormatCode.FormatterSettings = $Script:FormatterSettings
         }
-        $Data = Write-TextWithTime -Text "[+] Formatting file - $FilePath" {
+        $Data = Write-TextWithTime -Text "Formatting file - $FilePath" {
             try {
                 Invoke-Formatter -ScriptDefinition $Output -Settings $FormatCode.FormatterSettings #-Verbose:$false
             } catch {
                 $ErrorMessage = $_.Exception.Message
                 #Write-Warning "Merge module on file $FilePath failed. Error: $ErrorMessage"
                 Write-Host # This is to add new line, because the first line was opened up.
-                Write-Text "[-] Format-Code - Formatting on file $FilePath failed." -Color Red
-                Write-Text "[-] Format-Code - Error: $ErrorMessage" -Color Red
-                Write-Text "[-] Format-Code - This is most likely related to a bug in PSScriptAnalyzer running inside VSCode. Please try running outside of VSCode when using formatting." -Color Red
+                Write-Text "   [-] Format-Code - Formatting on file $FilePath failed." -Color Red
+                Write-Text "   [-] Format-Code - Error: $ErrorMessage" -Color Red
+                Write-Text "   [-] Format-Code - This is most likely related to a bug in PSScriptAnalyzer running inside VSCode. Please try running outside of VSCode when using formatting." -Color Red
                 return $false
             }
-        }
+        } -PreAppend Plus -SpacesBefore '   '
         if ($Data -eq $false) {
             return $false
         }
-        Write-TextWithTime -Text "[+] Saving file - $FilePath" {
+        Write-TextWithTime -Text "Saving file - $FilePath" {
             # Resave
             $Final = foreach ($O in $Data) {
                 if ($O.Trim() -ne '') {
@@ -57,6 +57,6 @@
                 Write-Text "[-] Format-Code - Resaving file $FilePath failed. Error: $ErrorMessage" -Color Red
                 return $false
             }
-        }
+        } -PreAppend Plus -SpacesBefore '   '
     }
 }
