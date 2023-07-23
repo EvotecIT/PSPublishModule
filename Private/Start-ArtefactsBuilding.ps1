1cf015c066b634a512bd122ad278a82532c3646f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-23 16:42:01 +0200

    Rebuild artefact building

--- Private/Start-ArtefactsBuilding.ps1
+++ Private/Start-ArtefactsBuilding.ps1
@@ -11,6 +11,7 @@
     if ($Artefact) {
         $Artefact = $ChosenArtefact
         $ChosenType = $Artefact.Type
+        $ID = if ($ChosenArtefact.ID) { $ChosenArtefact.ID } else { $null }
     } elseif ($Type) {
         if ($Configuration.Steps.BuildModule.$Type) {
             $Artefact = $Configuration.Steps.BuildModule.$Type
@@ -18,133 +19,150 @@
         } else {
             $Artefact = $null
         }
+        $ID = $null
     } else {
+        $ID = $null
         $Artefact = $null
     }
-    if ($Artefact -or $Artefact.Enabled) {
-        if ($Artefact -is [System.Collections.IDictionary]) {
-            if ($Artefact.Path) {
-                if ($Artefact.Relative -eq $false) {
-                    $FolderPathReleases = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Artefact.Path)
-                } else {
-                    $FolderPathReleases = [System.IO.Path]::Combine($FullProjectPath, $Artefact.Path)
-                }
-            } else {
-                $FolderPathReleases = [System.IO.Path]::Combine($FullProjectPath, $Type)
-            }
-        } else {
-            # default values
-            $FolderPathReleases = [System.IO.Path]::Combine($FullProjectPath, $Type)
-        }
-        # if ($DestinationPaths.Desktop) {
-        #     $CompressPath = [System.IO.Path]::Combine($DestinationPaths.Desktop, '*')
-        # } elseif ($DestinationPaths.Core) {
-        #     $CompressPath = [System.IO.Path]::Combine($DestinationPaths.Core, '*')
-        # }
 
-        # $compressArtefactSplat = @{
-        #     Configuration = $Configuration
-        #     LegacyName    = if ($Artefact -is [bool]) { $true } else { $false }
-        #     Source        = $CompressPath
-        #     Destination   = $FolderPathReleases
-        #     ModuleName    = $Configuration.Information.ModuleName
-        #     ModuleVersion = $Configuration.Information.Manifest.ModuleVersion
-        #     IncludeTag    = $Artefact.IncludeTagName
-        #     ArtefactName  = $Artefact.ArtefactName
-        # }
+    if ($ID) {
+        $TextToDisplay = "Preparing Artefact of type '$ChosenType' (ID: $ID)"
+    } else {
+        $TextToDisplay = "Preparing Artefact of type '$ChosenType'"
+    }
 
-        # $OutputArchive = Compress-Artefact @compressArtefactSplat
-        # if ($OutputArchive -eq $false) {
-        #     return $false
-        # }
+    # If artefact is not enabled, we don't want to do anything
+    if ($null -eq $Artefact -or $Artefact.Count -eq 0) {
+        return
+    }
 
-        #if ($Artefact) {
-        if ($Artefact.RequiredModules.ModulesPath) {
-            $DirectPathForPrimaryModule = $Artefact.RequiredModules.ModulesPath
-        } elseif ($Artefact.RequiredModules.Path) {
-            $DirectPathForPrimaryModule = $Artefact.RequiredModules.Path
-        } elseif ($Artefact.Path) {
-            $DirectPathForPrimaryModule = $Artefact.Path
-        } else {
-            $DirectPathForPrimaryModule = $FolderPathReleases
-        }
-        if ($Artefact.RequiredModules.Path) {
-            $DirectPathForRequiredModules = $Artefact.RequiredModules.Path
-        } elseif ($Artefact.RequiredModules.ModulesPath) {
-            $DirectPathForRequiredModules = $Artefact.RequiredModules.ModulesPath
-        } elseif ($Artefact.Path) {
-            $DirectPathForRequiredModules = $Artefact.Path
-        } else {
-            $DirectPathForRequiredModules = $FolderPathReleases
-        }
-        if ($Artefact -eq $true -or $Artefact.Enabled) {
+    Write-TextWithTime -Text $TextToDisplay -PreAppend Information -SpacesBefore '   ' {
+        if ($Artefact -or $Artefact.Enabled) {
             if ($Artefact -is [System.Collections.IDictionary]) {
-                if ($DirectPathForPrimaryModule) {
+                if ($Artefact.Path) {
                     if ($Artefact.Relative -eq $false) {
-                        $CurrentModulePath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($DirectPathForPrimaryModule)
+                        $FolderPathReleases = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Artefact.Path)
                     } else {
-                        $CurrentModulePath = [System.IO.Path]::Combine($FullProjectPath, $DirectPathForPrimaryModule)
+                        $FolderPathReleases = [System.IO.Path]::Combine($FullProjectPath, $Artefact.Path)
                     }
                 } else {
-                    $CurrentModulePath = [System.IO.Path]::Combine($FullProjectPath, 'ReleasesUnpacked')
+                    $FolderPathReleases = [System.IO.Path]::Combine($FullProjectPath, $Type)
                 }
-                if ($DirectPathForRequiredModules) {
-                    if ($Artefact.Relative -eq $false) {
-                        $RequiredModulesPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($DirectPathForRequiredModules)
-                    } else {
-                        $RequiredModulesPath = [System.IO.Path]::Combine($FullProjectPath, $DirectPathForRequiredModules)
-                    }
-                } else {
-                    $RequiredModulesPath = $ArtefactsPath
-                }
-                $ArtefactsPath = $Artefact.Path
-                # $FolderPathReleasesUnpacked = $ArtefactsPath
             } else {
                 # default values
-                $ArtefactsPath = [System.IO.Path]::Combine($FullProjectPath, 'ReleasesUnpacked', $TagName)
-                #$FolderPathReleasesUnpacked = [System.IO.Path]::Combine($FullProjectPath, 'ReleasesUnpacked', $TagName )
-                $RequiredModulesPath = $ArtefactsPath
-                $CurrentModulePath = $ArtefactsPath
+                $FolderPathReleases = [System.IO.Path]::Combine($FullProjectPath, $Type)
             }
+            # if ($DestinationPaths.Desktop) {
+            #     $CompressPath = [System.IO.Path]::Combine($DestinationPaths.Desktop, '*')
+            # } elseif ($DestinationPaths.Core) {
+            #     $CompressPath = [System.IO.Path]::Combine($DestinationPaths.Core, '*')
+            # }
 
-            # we try to set some defaults just in case some settings are not available (mainly because user is using non-DSL model)
-            # this is to make sure that if user is using relative paths we can still use them for copying files/folders
-            if ($null -eq $Artefact.DestinationFilesRelative) {
-                if ($null -ne $Artefact.Relative) {
-                    $Artefact.DestinationFilesRelative = $Artefact.Relative
-                }
-            }
-            if ($null -eq $Artefact.DestinationDirectoriesRelative) {
-                if ($null -ne $Artefact.Relative) {
-                    $Artefact.DestinationDirectoriesRelative = $Artefact.Relative
-                }
-            }
+            # $compressArtefactSplat = @{
+            #     Configuration = $Configuration
+            #     LegacyName    = if ($Artefact -is [bool]) { $true } else { $false }
+            #     Source        = $CompressPath
+            #     Destination   = $FolderPathReleases
+            #     ModuleName    = $Configuration.Information.ModuleName
+            #     ModuleVersion = $Configuration.Information.Manifest.ModuleVersion
+            #     IncludeTag    = $Artefact.IncludeTagName
+            #     ArtefactName  = $Artefact.ArtefactName
+            # }
 
-            $SplatArtefact = @{
-                ModuleName                     = $Configuration.Information.ModuleName
-                ModuleVersion                  = $Configuration.Information.Manifest.ModuleVersion
-                LegacyName                     = if ($Artefact -is [bool]) { $true } else { $false }
-                CopyMainModule                 = $true
-                CopyRequiredModules            = $Artefact.RequiredModules -eq $true -or $Artefact.RequiredModules.Enabled
-                ProjectPath                    = $FullProjectPath
-                Destination                    = $ArtefactsPath
-                DestinationMainModule          = $CurrentModulePath
-                DestinationRequiredModules     = $RequiredModulesPath
-                RequiredModules                = $Configuration.Information.Manifest.RequiredModules
-                Files                          = $Artefact.FilesOutput
-                Folders                        = $Artefact.DirectoryOutput
-                DestinationFilesRelative       = $Artefact.DestinationFilesRelative
-                DestinationDirectoriesRelative = $Artefact.DestinationDirectoriesRelative
-                Configuration                  = $Configuration
-                IncludeTag                     = $Artefact.IncludeTagName
-                ArtefactName                   = $Artefact.ArtefactName
-                ZipIt                          = if ($ChosenType -in 'Packed', 'Releases', 'ScriptPacked') { $true } else { $false }
-                ConvertToScript                = if ($ChosenType -in 'ScriptPacked', 'Script') { $true } else { $false }
-                DestinationZip                 = $CurrentModulePath
-                ScriptMerge                    = $Artefact.ScriptMerge
+            # $OutputArchive = Compress-Artefact @compressArtefactSplat
+            # if ($OutputArchive -eq $false) {
+            #     return $false
+            # }
+
+            #if ($Artefact) {
+            if ($Artefact.RequiredModules.ModulesPath) {
+                $DirectPathForPrimaryModule = $Artefact.RequiredModules.ModulesPath
+            } elseif ($Artefact.RequiredModules.Path) {
+                $DirectPathForPrimaryModule = $Artefact.RequiredModules.Path
+            } elseif ($Artefact.Path) {
+                $DirectPathForPrimaryModule = $Artefact.Path
+            } else {
+                $DirectPathForPrimaryModule = $FolderPathReleases
             }
-            Remove-EmptyValue -Hashtable $SplatArtefact
-            Add-Artefact @SplatArtefact
+            if ($Artefact.RequiredModules.Path) {
+                $DirectPathForRequiredModules = $Artefact.RequiredModules.Path
+            } elseif ($Artefact.RequiredModules.ModulesPath) {
+                $DirectPathForRequiredModules = $Artefact.RequiredModules.ModulesPath
+            } elseif ($Artefact.Path) {
+                $DirectPathForRequiredModules = $Artefact.Path
+            } else {
+                $DirectPathForRequiredModules = $FolderPathReleases
+            }
+            if ($Artefact -eq $true -or $Artefact.Enabled) {
+                if ($Artefact -is [System.Collections.IDictionary]) {
+                    if ($DirectPathForPrimaryModule) {
+                        if ($Artefact.Relative -eq $false) {
+                            $CurrentModulePath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($DirectPathForPrimaryModule)
+                        } else {
+                            $CurrentModulePath = [System.IO.Path]::Combine($FullProjectPath, $DirectPathForPrimaryModule)
+                        }
+                    } else {
+                        $CurrentModulePath = [System.IO.Path]::Combine($FullProjectPath, 'ReleasesUnpacked')
+                    }
+                    if ($DirectPathForRequiredModules) {
+                        if ($Artefact.Relative -eq $false) {
+                            $RequiredModulesPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($DirectPathForRequiredModules)
+                        } else {
+                            $RequiredModulesPath = [System.IO.Path]::Combine($FullProjectPath, $DirectPathForRequiredModules)
+                        }
+                    } else {
+                        $RequiredModulesPath = $ArtefactsPath
+                    }
+                    $ArtefactsPath = $Artefact.Path
+                    # $FolderPathReleasesUnpacked = $ArtefactsPath
+                } else {
+                    # default values
+                    $ArtefactsPath = [System.IO.Path]::Combine($FullProjectPath, 'ReleasesUnpacked', $TagName)
+                    #$FolderPathReleasesUnpacked = [System.IO.Path]::Combine($FullProjectPath, 'ReleasesUnpacked', $TagName )
+                    $RequiredModulesPath = $ArtefactsPath
+                    $CurrentModulePath = $ArtefactsPath
+                }
+
+                # we try to set some defaults just in case some settings are not available (mainly because user is using non-DSL model)
+                # this is to make sure that if user is using relative paths we can still use them for copying files/folders
+                if ($null -eq $Artefact.DestinationFilesRelative) {
+                    if ($null -ne $Artefact.Relative) {
+                        $Artefact.DestinationFilesRelative = $Artefact.Relative
+                    }
+                }
+                if ($null -eq $Artefact.DestinationDirectoriesRelative) {
+                    if ($null -ne $Artefact.Relative) {
+                        $Artefact.DestinationDirectoriesRelative = $Artefact.Relative
+                    }
+                }
+
+                $SplatArtefact = @{
+                    ModuleName                     = $Configuration.Information.ModuleName
+                    ModuleVersion                  = $Configuration.Information.Manifest.ModuleVersion
+                    LegacyName                     = if ($Artefact -is [bool]) { $true } else { $false }
+                    CopyMainModule                 = $true
+                    CopyRequiredModules            = $Artefact.RequiredModules -eq $true -or $Artefact.RequiredModules.Enabled
+                    ProjectPath                    = $FullProjectPath
+                    Destination                    = $ArtefactsPath
+                    DestinationMainModule          = $CurrentModulePath
+                    DestinationRequiredModules     = $RequiredModulesPath
+                    RequiredModules                = $Configuration.Information.Manifest.RequiredModules
+                    Files                          = $Artefact.FilesOutput
+                    Folders                        = $Artefact.DirectoryOutput
+                    DestinationFilesRelative       = $Artefact.DestinationFilesRelative
+                    DestinationDirectoriesRelative = $Artefact.DestinationDirectoriesRelative
+                    Configuration                  = $Configuration
+                    IncludeTag                     = $Artefact.IncludeTagName
+                    ArtefactName                   = $Artefact.ArtefactName
+                    ZipIt                          = if ($ChosenType -in 'Packed', 'Releases', 'ScriptPacked') { $true } else { $false }
+                    ConvertToScript                = if ($ChosenType -in 'ScriptPacked', 'Script') { $true } else { $false }
+                    DestinationZip                 = $CurrentModulePath
+                    ScriptMerge                    = $Artefact.ScriptMerge
+                    ID                             = if ($ChosenArtefact.ID) { $ChosenArtefact.ID } else { $null }
+                }
+                Remove-EmptyValue -Hashtable $SplatArtefact
+                Add-Artefact @SplatArtefact
+            }
         }
-    }
+    } -ColorBefore Yellow -ColorTime Yellow -Color Yellow
 }
