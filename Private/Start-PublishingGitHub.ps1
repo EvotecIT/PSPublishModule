11646c1288fcdf4bfec9b9e696eeddb7bc2a9f8c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-24 13:00:32 +0200

    Improve publishing to github

--- Private/Start-PublishingGitHub.ps1
+++ Private/Start-PublishingGitHub.ps1
@@ -6,19 +6,30 @@
         [string] $ProjectName
     )
     if ($ChosenNuget) {
-        if ($ChosenNuget.Id) {
-            if ($Configuration.CurrentSettings['Artefact'][$ChosenNuget.Id]) {
-                #$ZipName = $Configuration.CurrentSettings['Artefact'][$ChosenNuget.Id].ZipName
-                $ZipPath = $Configuration.CurrentSettings['Artefact'][$ChosenNuget.Id].ZipPath
-            } else {
-                $ZipPath = $null
+        [Array] $ListZips = if ($ChosenNuget.Id) {
+            foreach ($Zip in $Configuration.CurrentSettings['Artefact']) {
+                if ($Zip.Id -eq $ChosenNuget.Id) {
+                    $ZipPath = $Zip.ZipPath
+                    if ($ZipPath -and (Test-Path -LiteralPath $ZipPath)) {
+                        $ZipPath
+                    }
+                }
             }
+            # if ($Configuration.CurrentSettings['Artefact'][$ChosenNuget.Id]) {
+            #     #$ZipName = $Configuration.CurrentSettings['Artefact'][$ChosenNuget.Id].ZipName
+            #     $ZipPath = $Configuration.CurrentSettings['Artefact'][$ChosenNuget.Id].ZipPath
+            # } else {
+            #     $ZipPath = $null
+            # }
         } else {
             if ($Configuration.CurrentSettings['ArtefactDefault']) {
                 #$ZipName = $Configuration.CurrentSettings['ArtefactDefault'].ZipName
                 $ZipPath = $Configuration.CurrentSettings['ArtefactDefault'].ZipPath
+                if ($ZipPath -and (Test-Path -LiteralPath $ZipPath)) {
+                    $ZipPath
+                }
             } else {
-                $ZipPath = $null
+                #$ZipPath = $null
             }
         }
         if ($ChosenNuget.ID) {
@@ -26,53 +37,58 @@
         } else {
             $TextToUse = "Publishing to GitHub ($ZipPath)"
         }
-        Write-TextWithTime -Text $TextToUse -PreAppend Information -ColorBefore Yellow {
-            if ($ZipPath -and (Test-Path -LiteralPath $ZipPath)) {
-                if ($ChosenNuget.OverwriteTagName) {
-                    $TagName = $ChosenNuget.OverwriteTagName
+        if ($ListZips.Count -gt 0) {
+            Write-TextWithTime -Text $TextToUse -PreAppend Information -ColorBefore Yellow {
+                if ($ZipPath -and (Test-Path -LiteralPath $ZipPath)) {
+                    if ($ChosenNuget.OverwriteTagName) {
+                        $TagName = $ChosenNuget.OverwriteTagName
+                    } else {
+                        $TagName = "v$($Configuration.Information.Manifest.ModuleVersion)"
+                    }
+                    $sendGitHubReleaseSplat = [ordered] @{
+                        GitHubUsername       = $ChosenNuget.UserName
+                        GitHubRepositoryName = if ($ChosenNuget.RepositoryName) { $ChosenNuget.RepositoryName } else { $ProjectName }
+                        GitHubAccessToken    = $ChosenNuget.ApiKey
+                        TagName              = $TagName
+                        AssetFilePaths       = $ListZips
+                        # settable via PSD1 manifest
+                        IsPreRelease         = if ($Configuration.CurrentSettings.Prerelease) { $true } else { $false }
+                        # those don't work, requires testing
+                        #GenerateReleaseNotes = $true
+                        #MakeLatest           = $true
+                        #GenerateReleaseNotes = if ($Configuration.Options.GitHub.GenerateReleaseNotes) { $true } else { $false }
+                        #MakeLatest           = if ($Configuration.Options.GitHub.MakeLatest) { $true } else { $false }
+                        Verbose              = if ($ChosenNuget.Verbose) { $ChosenNuget.Verbose } else { $false }
+                    }
+
+                    $StatusGithub = Send-GitHubRelease @sendGitHubReleaseSplat
+
+                    if ($StatusGithub.ReleaseCreationSucceeded -and $statusGithub.Succeeded) {
+                        $GithubColor = 'Green'
+                        $GitHubText = '+'
+                    } else {
+                        $GithubColor = 'Red'
+                        $GitHubText = '-'
+                    }
+
+                    Write-Text "[$GitHubText] GitHub Release Creation Status: $($StatusGithub.ReleaseCreationSucceeded)" -Color $GithubColor
+                    Write-Text "[$GitHubText] GitHub Release Succeeded: $($statusGithub.Succeeded)" -Color $GithubColor
+                    Write-Text "[$GitHubText] GitHub Release Asset Upload Succeeded: $($statusGithub.AllAssetUploadsSucceeded)" -Color $GithubColor
+                    Write-Text "[$GitHubText] GitHub Release URL: $($statusGitHub.ReleaseUrl)" -Color $GithubColor
+                    if ($statusGithub.ErrorMessage) {
+                        Write-Text "[$GitHubText] GitHub Release ErrorMessage: $($statusGithub.ErrorMessage)" -Color $GithubColor
+                        return $false
+                    }
+
                 } else {
-                    $TagName = "v$($Configuration.Information.Manifest.ModuleVersion)"
-                }
-                $sendGitHubReleaseSplat = [ordered] @{
-                    GitHubUsername       = $ChosenNuget.UserName
-                    GitHubRepositoryName = if ($ChosenNuget.RepositoryName) { $ChosenNuget.RepositoryName } else { $ProjectName }
-                    GitHubAccessToken    = $ChosenNuget.ApiKey
-                    TagName              = $TagName
-                    AssetFilePaths       = $ZipPath
-                    # settable via PSD1 manifest
-                    IsPreRelease         = if ($Configuration.CurrentSettings.Prerelease) { $true } else { $false }
-                    # those don't work, requires testing
-                    #GenerateReleaseNotes = $true
-                    #MakeLatest           = $true
-                    #GenerateReleaseNotes = if ($Configuration.Options.GitHub.GenerateReleaseNotes) { $true } else { $false }
-                    #MakeLatest           = if ($Configuration.Options.GitHub.MakeLatest) { $true } else { $false }
-                    Verbose              = if ($ChosenNuget.Verbose) { $ChosenNuget.Verbose } else { $false }
-                }
-
-                $StatusGithub = Send-GitHubRelease @sendGitHubReleaseSplat
-
-                if ($StatusGithub.ReleaseCreationSucceeded -and $statusGithub.Succeeded) {
-                    $GithubColor = 'Green'
-                    $GitHubText = '+'
-                } else {
-                    $GithubColor = 'Red'
-                    $GitHubText = '-'
-                }
-
-                Write-Text "[$GitHubText] GitHub Release Creation Status: $($StatusGithub.ReleaseCreationSucceeded)" -Color $GithubColor
-                Write-Text "[$GitHubText] GitHub Release Succeeded: $($statusGithub.Succeeded)" -Color $GithubColor
-                Write-Text "[$GitHubText] GitHub Release Asset Upload Succeeded: $($statusGithub.AllAssetUploadsSucceeded)" -Color $GithubColor
-                Write-Text "[$GitHubText] GitHub Release URL: $($statusGitHub.ReleaseUrl)" -Color $GithubColor
-                if ($statusGithub.ErrorMessage) {
-                    Write-Text "[$GitHubText] GitHub Release ErrorMessage: $($statusGithub.ErrorMessage)" -Color $GithubColor
+                    Write-Text "   [e] GitHub Release Creation Status: Failed" -Color Red
+                    Write-Text "   [e] GitHub Release Creation Reason: $ZipPath doesn't exists. Most likely Releases option is disabled." -Color Red
                     return $false
                 }
-
-            } else {
-                Write-Text "   [e] GitHub Release Creation Status: Failed" -Color Red
-                Write-Text "   [e] GitHub Release Creation Reason: $ZipPath doesn't exists. Most likely Releases option is disabled." -Color Red
-                return $false
             }
+        } else {
+            Write-Text -Text "[-] Publishing to GitHub failed. No ZIPs to process." -Color Red
+            return $false
         }
     } else {
         # old configuration
