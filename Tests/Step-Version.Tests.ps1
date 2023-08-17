e8f0072df87f18e7eef064250de956604d48a1a7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-17 16:16:37 +0200

    Update test

--- Tests/Step-Version.Tests.ps1
+++ Tests/Step-Version.Tests.ps1
@@ -1,13 +1,13 @@
 ﻿Describe 'Step-Version' {
     It 'Testing version 0.1.X' {
-        $Features = Import-Module $PSScriptRoot\..\PSPublishModule.psd1 -PassThru
+        $Features = Import-Module "PSPublishModule" -PassThru
         $Output = & $Features {
             Step-Version -Module 'PowerShellManager' -ExpectedVersion '0.1.X'
         }
         $Output | Should -Be "0.1.3"
     }
     It "Testing version 0.2.X" {
-        $Features = Import-Module $PSScriptRoot\..\PSPublishModule.psd1 -PassThru
+        $Features = Import-Module "PSPublishModule" -PassThru
         $Output = & $Features {
             Step-Version -Module 'PowerShellManager' -ExpectedVersion '0.2.X'
         }
