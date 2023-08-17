d342241f6068c311e272ff8494745cb02a8448f4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-17 19:16:18 +0200

    Update test

--- Tests/Step-Version.Tests.ps1
+++ Tests/Step-Version.Tests.ps1
@@ -1,13 +1,23 @@
 ﻿Describe 'Step-Version' {
     It 'Testing version 0.1.X' {
-        $Features = Import-Module "PSPublishModule" -PassThru
+        if (-not (Get-Module -ListAvailable -Name 'PSPublishModule')) {
+            $ModuleToLoad = "$PSScriptRoot\..\PSPublishModule\PSPublishModule.psd1"
+        } else {
+            $ModuleToLoad = 'PSPublishModule'
+        }
+        $Features = Import-Module $ModuleToLoad -PassThru
         $Output = & $Features {
             Step-Version -Module 'PowerShellManager' -ExpectedVersion '0.1.X'
         }
         $Output | Should -Be "0.1.3"
     }
     It "Testing version 0.2.X" {
-        $Features = Import-Module "PSPublishModule" -PassThru
+        if (-not (Get-Module -ListAvailable -Name 'PSPublishModule')) {
+            $ModuleToLoad = "$PSScriptRoot\..\PSPublishModule\PSPublishModule.psd1"
+        } else {
+            $ModuleToLoad = 'PSPublishModule'
+        }
+        $Features = Import-Module $ModuleToLoad -PassThru
         $Output = & $Features {
             Step-Version -Module 'PowerShellManager' -ExpectedVersion '0.2.X'
         }
