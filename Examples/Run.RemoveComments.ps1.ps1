1dcb18cbb363bf5e44e832d5dd12bd67f0f36115
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-19 12:12:37 +0200

    Update example

---
+++ Examples/Run.RemoveComments.ps1.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module .\PSPublishModule.psd1 -Force
+
+$removeCommentsSplat = @{
+    SourceFilePath      = 'C:\Users\przemyslaw.klys\OneDrive - Evotec Services sp. z o.o\Documents\PowerShell\Modules\PSWriteHTML\PSWriteHTML.psm1'
+    DestinationFilePath = 'C:\Support\GitHub\PSPublishModule\Examples\TestScript1.ps1'
+}
+
+#Remove-Comments @removeCommentsSplat -RemoveCommentsInParamBlock -RemoveAllEmptyLines -RemoveCommentsBeforeParamBlock
+
+$removeCommentsSplat = @{
+    SourceFilePath                 = 'C:\Support\GitHub\PSPublishModule\Examples\TestScript.ps1'
+    DestinationFilePath            = 'C:\Support\GitHub\PSPublishModule\Examples\TestScript1.ps1'
+    RemoveAllEmptyLines            = $true
+    RemoveCommentsInParamBlock     = $true
+    RemoveCommentsBeforeParamBlock = $true
+}
+
+Remove-Comments @removeCommentsSplat
