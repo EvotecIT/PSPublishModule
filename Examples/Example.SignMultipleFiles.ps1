8e97211d5e086779e72fe89aef4b57e4274433ed
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-19 22:09:14 +0100

    Example for signing all files in a folder

---
+++ Examples/Example.SignMultipleFiles.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSPublishModule.psd1 -Force
+
+$Folder = "C:\Users\przemyslaw.klys\Downloads\MDE\MDE\2016"
+
+$registerCertificateSplat = @{
+    LocalStore      = 'CurrentUser'
+    Path            = $Folder
+    Include         = @('*.ps1', '*.psd1', '*.psm1', '*.dll', '*.cat')
+    TimeStampServer = 'http://timestamp.digicert.com'
+    Thumbprint      = '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+}
+
+Register-Certificate @registerCertificateSplat -Verbose
