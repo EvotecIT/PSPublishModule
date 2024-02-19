02ce5b9a4668501c8d69657895da6c10e7bf0a5c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-19 21:15:48 +0100

    Better code output processing

--- Private/New-LibraryContent.ps1
+++ Private/New-LibraryContent.ps1
@@ -4,27 +4,103 @@
         [string[]] $LibrariesStandard,
         [string[]] $LibrariesCore,
         [string[]] $LibrariesDefault,
-        [System.Collections.IDictionary] $Configuration
+        [System.Collections.IDictionary] $Configuration,
+        [switch] $OptimizedLoading
     )
+    if ($Configuration.Steps.BuildLibraries.HandleAssemblyWithSameName) {
+        $Handle = $Configuration.Steps.BuildLibraries.HandleAssemblyWithSameName
+    } else {
+        $Handle = $false
+    }
 
-    $LibraryContent = @(
-        if ($LibrariesStandard.Count -gt 0) {
-            :nextFile foreach ($File in $LibrariesStandard) {
-                $Extension = $File.Substring($File.Length - 4, 4)
-                if ($Extension -eq '.dll') {
-                    foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
-                        if ($File -like "*\$IgnoredFile") {
-                            continue nextFile
+    if ($OptimizedLoading) {
+        $LibraryContent = @(
+            if ($LibrariesStandard.Count -gt 0) {
+                $Files = :nextFile foreach ($File in $LibrariesStandard) {
+                    $Extension = $File.Substring($File.Length - 4, 4)
+                    if ($Extension -eq '.dll') {
+                        foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                            if ($File -like "*\$IgnoredFile") {
+                                continue nextFile
+                            }
+                        }
+                        $File
+                    }
+                }
+                $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $Files -HandleAssemblyWithSameName $Handle
+                $Output
+            } elseif ($LibrariesCore.Count -gt 0 -and $LibrariesDefault.Count -gt 0) {
+                'if ($PSEdition -eq ''Core'') {'
+                if ($LibrariesCore.Count -gt 0) {
+                    $Files = :nextFile foreach ($File in $LibrariesCore) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
+                            }
+                            $File
                         }
                     }
-                    $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File
+                    $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $Files -HandleAssemblyWithSameName $Handle
+                    $Output
+                }
+                '} else {'
+                if ($LibrariesDefault.Count -gt 0) {
+                    $Files = :nextFile foreach ($File in $LibrariesDefault) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
+                            }
+                            $File
+                        }
+                    }
+                    $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $Files -HandleAssemblyWithSameName $Handle
+                    $Output
+                }
+                '}'
+            } else {
+                if ($LibrariesCore.Count -gt 0) {
+                    $Files = :nextFile foreach ($File in $LibrariesCore) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
+                            }
+                            $File
+                        }
+                    }
+                    $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $Files -HandleAssemblyWithSameName $Handle
+                    $Output
+                }
+                if ($LibrariesDefault.Count -gt 0) {
+                    $Files = :nextFile foreach ($File in $LibrariesDefault) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
+                            }
+                            $File
+                        }
+                    }
+                    $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $Files -HandleAssemblyWithSameName $Handle
                     $Output
                 }
             }
-        } elseif ($LibrariesCore.Count -gt 0 -and $LibrariesDefault.Count -gt 0) {
-            'if ($PSEdition -eq ''Core'') {'
-            if ($LibrariesCore.Count -gt 0) {
-                :nextFile foreach ($File in $LibrariesCore) {
+        )
+    } else {
+
+        $LibraryContent = @(
+            if ($LibrariesStandard.Count -gt 0) {
+                :nextFile foreach ($File in $LibrariesStandard) {
                     $Extension = $File.Substring($File.Length - 4, 4)
                     if ($Extension -eq '.dll') {
                         foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
@@ -32,57 +108,73 @@
                                 continue nextFile
                             }
                         }
-                        $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File
+                        $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File -HandleAssemblyWithSameName $Handle
                         $Output
                     }
                 }
-            }
-            '} else {'
-            if ($LibrariesDefault.Count -gt 0) {
-                :nextFile foreach ($File in $LibrariesDefault) {
-                    $Extension = $File.Substring($File.Length - 4, 4)
-                    if ($Extension -eq '.dll') {
-                        foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
-                            if ($File -like "*\$IgnoredFile") {
-                                continue nextFile
+            } elseif ($LibrariesCore.Count -gt 0 -and $LibrariesDefault.Count -gt 0) {
+                'if ($PSEdition -eq ''Core'') {'
+                if ($LibrariesCore.Count -gt 0) {
+                    :nextFile foreach ($File in $LibrariesCore) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
                             }
+                            $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File -HandleAssemblyWithSameName $Handle
+                            $Output
                         }
-                        $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File
-                        $Output
                     }
                 }
-            }
-            '}'
-        } else {
-            if ($LibrariesCore.Count -gt 0) {
-                :nextFile foreach ($File in $LibrariesCore) {
-                    $Extension = $File.Substring($File.Length - 4, 4)
-                    if ($Extension -eq '.dll') {
-                        foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
-                            if ($File -like "*\$IgnoredFile") {
-                                continue nextFile
+                '} else {'
+                if ($LibrariesDefault.Count -gt 0) {
+                    :nextFile foreach ($File in $LibrariesDefault) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
                             }
+                            $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File -HandleAssemblyWithSameName $Handle
+                            $Output
                         }
-                        $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File
-                        $Output
                     }
                 }
-            }
-            if ($LibrariesDefault.Count -gt 0) {
-                :nextFile foreach ($File in $LibrariesDefault) {
-                    $Extension = $File.Substring($File.Length - 4, 4)
-                    if ($Extension -eq '.dll') {
-                        foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
-                            if ($File -like "*\$IgnoredFile") {
-                                continue nextFile
+                '}'
+            } else {
+                if ($LibrariesCore.Count -gt 0) {
+                    :nextFile foreach ($File in $LibrariesCore) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
                             }
+                            $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File -HandleAssemblyWithSameName $Handle
+                            $Output
                         }
-                        $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File
-                        $Output
+                    }
+                }
+                if ($LibrariesDefault.Count -gt 0) {
+                    :nextFile foreach ($File in $LibrariesDefault) {
+                        $Extension = $File.Substring($File.Length - 4, 4)
+                        if ($Extension -eq '.dll') {
+                            foreach ($IgnoredFile in $Configuration.Steps.BuildLibraries.IgnoreLibraryOnLoad) {
+                                if ($File -like "*\$IgnoredFile") {
+                                    continue nextFile
+                                }
+                            }
+                            $Output = New-DLLCodeOutput -DebugDLL $Configuration.Steps.BuildModule.DebugDLL -File $File -HandleAssemblyWithSameName $Handle
+                            $Output
+                        }
                     }
                 }
             }
-        }
-    )
+        )
+    }
     $LibraryContent
 }
