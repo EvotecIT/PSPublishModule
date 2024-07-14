ad59d37bc8712a02e7069cb15ed9287563771b47
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-14 19:45:37 +0200

    Improve write-text with preappend and spaces

--- Private/Write-Text.ps1
+++ Private/Write-Text.ps1
@@ -1,13 +1,39 @@
 ﻿function Write-Text {
     [CmdletBinding()]
     param(
-        [string] $Text,
+        [Parameter(Position = 0)][string] $Text,
         [System.ConsoleColor] $Color = [System.ConsoleColor]::Cyan,
         [System.ConsoleColor] $ColorTime = [System.ConsoleColor]::Green,
         [switch] $Start,
         [switch] $End,
-        [System.Diagnostics.Stopwatch] $Time
+        [System.Diagnostics.Stopwatch] $Time,
+        [ValidateSet('Plus', 'Minus', 'Information', 'Addition')][string] $PreAppend,
+        [string] $SpacesBefore
     )
+    if ($PreAppend) {
+        if ($PreAppend -eq "Information") {
+            $TextBefore = "$SpacesBefore[i] "
+            if (-not $ColorBefore) {
+                $ColorBefore = [System.ConsoleColor]::Yellow
+            }
+        } elseif ($PreAppend -eq 'Minus') {
+            $TextBefore = "$SpacesBefore[-] "
+            if (-not $ColorBefore) {
+                $ColorBefore = [System.ConsoleColor]::Red
+            }
+        } elseif ($PreAppend -eq 'Plus') {
+            $TextBefore = "$SpacesBefore[+] "
+            if (-not $ColorBefore) {
+                $ColorBefore = [System.ConsoleColor]::Cyan
+            }
+        } elseif ($PreAppend -eq 'Addition') {
+            $TextBefore = "$SpacesBefore[>] "
+            if (-not $ColorBefore) {
+                $ColorBefore = [System.ConsoleColor]::Yellow
+            }
+        }
+        Write-Host -Object "$TextBefore" -NoNewline -ForegroundColor $ColorBefore
+    }
     if (-not $Start -and -not $End) {
         Write-Host "$Text" -ForegroundColor $Color
     }
