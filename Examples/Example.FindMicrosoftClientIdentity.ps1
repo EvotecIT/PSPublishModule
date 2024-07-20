8f4f3b547ed02b33474216ce94499dea43ab3863
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-20 22:04:22 +0200

    Detect identity

---
+++ Examples/Example.FindMicrosoftClientIdentity.ps1
@@ -0,0 +1,27 @@
+﻿$Folder = $Env:PSModulePath -split ";"
+foreach ($F in $Folder) {
+    if (Test-Path -Path $F) {
+        Get-ChildItem -LiteralPath $F -Recurse -Filter "*.dll" | ForEach-Object {
+            if ($_.Name -like "Microsoft.Identity.Client.dll") {
+                [PSCustomObject] @{
+                    Name    = $_.FullName
+                    Version = $_.VersionInfo.FileVersion
+                }
+            }
+        }
+    }
+}
+
+# Could not load type 'Microsoft.Identity.Client.Extensions.Msal.MsalCachePersistenceException
+foreach ($F in $Folder) {
+    if (Test-Path -Path $F) {
+        Get-ChildItem -LiteralPath $F -Recurse -Filter "*.dll" | ForEach-Object {
+            if ($_.Name -like "Microsoft.Identity.Client.*") {
+                [PSCustomObject] @{
+                    Name    = $_.FullName
+                    Version = $_.VersionInfo.FileVersion
+                }
+            }
+        }
+    }
+}
