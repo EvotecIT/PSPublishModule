75a9bbef3aea60b99b366a0dc92ce48fe612ac12
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-10-18 18:55:35 +0200

    Add better error handling

--- Private/Remove-Directory.ps1
+++ Private/Remove-Directory.ps1
@@ -1,16 +1,17 @@
 function Remove-Directory {
     [CmdletBinding()]
     param (
-        [string] $Directory
+        [string] $Directory,
+        [string] $SpacesBefore
     )
     if ($Directory) {
         $Exists = Test-Path -LiteralPath $Directory
         if ($Exists) {
             try {
-                Remove-Item -Path $Directory -Confirm:$false -Recurse -Force -ErrorAction Stop
+                Remove-Item -Path $Directory -ErrorAction Stop -Force -Recurse -Confirm:$false
             } catch {
                 $ErrorMessage = $_.Exception.Message
-                Write-Text "[e] Can't delete folder $Directory. Fix error before continuing: $ErrorMessage" -Color Red
+                Write-Text "Can't delete folder $Directory. Fix error before continuing: $ErrorMessage" -PreAppend Error -SpacesBefore $SpacesBefore
                 return $false
             }
         }
