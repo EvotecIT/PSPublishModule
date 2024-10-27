cf328f4d6b70b2cad7a55836ae6acc12af4b09cc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-10-27 19:18:35 +0100

    Add docs

--- Public/New-ConfigurationImportModule.ps1
+++ Public/New-ConfigurationImportModule.ps1
@@ -1,4 +1,23 @@
 ﻿function New-ConfigurationImportModule {
+    <#
+    .SYNOPSIS
+    Creates a configuration for importing PowerShell modules.
+
+    .DESCRIPTION
+    This function generates a configuration object for importing PowerShell modules. It allows specifying whether to import the current module itself and/or any required modules.
+
+    .PARAMETER ImportSelf
+    Indicates whether to import the current module itself.
+
+    .PARAMETER ImportRequiredModules
+    Indicates whether to import any required modules specified in the module manifest.
+
+    .EXAMPLE
+    New-ConfigurationImportModule -ImportSelf -ImportRequiredModules
+
+    .NOTES
+    This function helps in creating a standardized import configuration for PowerShell modules.
+    #>
     [CmdletBinding()]
     param(
         [switch] $ImportSelf,
