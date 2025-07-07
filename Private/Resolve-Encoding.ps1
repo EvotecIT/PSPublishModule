8b430b178f59dfdfada443c11050d511d0abe94e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-07 09:46:46 +0200

    feat: ✨ update `Resolve-Encoding` function to support 'Any' encoding
    * Added 'Any' to the list of valid encodings in the `ValidateSet`.
    * Updated documentation to clarify that 'Any' returns null for conversion functions.

--- Private/Resolve-Encoding.ps1
+++ Private/Resolve-Encoding.ps1
@@ -10,6 +10,7 @@ function Resolve-Encoding {
 
     .PARAMETER Name
         The name of the encoding to resolve. Supports common encodings used in text file processing.
+        'Any' is a special value that returns null - used for converting from any encoding.
 
     .EXAMPLE
         Resolve-Encoding -Name 'UTF8BOM'
@@ -18,11 +19,15 @@ function Resolve-Encoding {
     .EXAMPLE
         Resolve-Encoding -Name 'ASCII'
         Returns the ASCII encoding object.
+
+    .EXAMPLE
+        Resolve-Encoding -Name 'Any'
+        Returns null - used to indicate "any source encoding" in conversion functions.
     #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
-        [ValidateSet('Ascii','BigEndianUnicode','Unicode','UTF7','UTF8','UTF8BOM','UTF32','Default','OEM')]
+        [ValidateSet('Ascii','BigEndianUnicode','Unicode','UTF7','UTF8','UTF8BOM','UTF32','Default','OEM','Any')]
         [string] $Name
     )
 
@@ -35,6 +40,10 @@ function Resolve-Encoding {
         }
         'OEM' {
             return [System.Text.Encoding]::GetEncoding([Console]::OutputEncoding.CodePage)
+        }
+        'ANY' {
+            # Return null for 'Any' - caller will handle this case
+            return $null
         }
         default {
             try {
