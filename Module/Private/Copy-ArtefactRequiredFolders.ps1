4123b00c41a5d450d1468950d9c70c65d64008c0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-12-26 13:53:45 +0100

    🗑️ chore: remove deprecated private functions
    - Deleted `Remove-ItemAlternative`, `Repair-CustomPlaceHolders`, `Resolve-DocFile`, `Resolve-Encoding`, `Script.FormatterSettings`, `Test-Basic`, `Test-ModuleImport`, `Test-ReparsePoint`, `Test-RequiredModules`, `Test-ScriptFile`, `Test-ScriptModule`, `Write-Heading`, `Write-PowerShellHashTable`, `Write-Text`, and `Write-TextWithTime` functions.
    - These functions were no longer needed and their removal helps to clean up the codebase.

--- Module/Private/Copy-ArtefactRequiredFolders.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Copy-ArtefactRequiredFolders {
-    [CmdletBinding()]
-    param(
-        [System.Collections.IDictionary] $FoldersInput,
-        [string] $ProjectPath,
-        [string] $Destination,
-        [nullable[bool]] $DestinationRelative
-    )
-}
--- Module/Private/Write-Heading.ps1
+++
@@ -1,8 +0,0 @@
-function Write-Heading {
-    [CmdletBinding()]
-    param([Parameter(Mandatory)][string] $Text)
-    Write-Host ('=' * $Text.Length) -ForegroundColor DarkGray
-    Write-Host $Text -ForegroundColor Cyan
-    Write-Host ('=' * $Text.Length) -ForegroundColor DarkGray
-}
-
