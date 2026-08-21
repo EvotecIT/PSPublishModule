2359e24978c8f589a74b099bc4b75a9e00103596
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-08-21 21:45:42 +0200

    docs: 📝 Update EventViewerX example path

--- Module/Examples/Example.CmdletAliasDetection.ps1
+++ Module/Examples/Example.CmdletAliasDetection.ps1
@@ -2,5 +2,5 @@
 
 #Get-PowerShellAssemblyMetadata -Path "C:\Support\GitHub\Mailozaurr\Sources\Mailozaurr.PowerShell\bin\Release\net472\Mailozaurr.PowerShell.dll" | Format-Table
 #Get-PowerShellAssemblyMetadata -Path "C:\Support\GitHub\Mailozaurr\Sources\Mailozaurr.PowerShell\bin\Release\net7.0\Mailozaurr.PowerShell.dll"
-#Get-PowerShellAssemblyMetadata -Path "C:\Support\GitHub\PSEventViewer\Sources\PSEventViewer\bin\Debug\net6.0\PSEventViewer.dll" | Format-Table
-Get-PowerShellAssemblyMetadata -Path "C:\Support\GitHub\PSEventViewer\Sources\PSEventViewer\bin\Debug\net472\PSEventViewer.dll" -Verbose | Format-Table
+#Get-PowerShellAssemblyMetadata -Path "C:\Support\GitHub\EventViewerX\Sources\PSEventViewer\bin\Debug\net8.0-windows\PSEventViewer.dll" | Format-Table
+Get-PowerShellAssemblyMetadata -Path "C:\Support\GitHub\EventViewerX\Sources\PSEventViewer\bin\Debug\net472\PSEventViewer.dll" -Verbose | Format-Table
