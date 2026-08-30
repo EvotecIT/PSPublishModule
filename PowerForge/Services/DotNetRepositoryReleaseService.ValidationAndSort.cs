0c44782b64ee8ba038001dc7a055b4614df1ce41
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-08-30 09:56:14 +0200

    Keep publish validation focused

--- PowerForge/Services/DotNetRepositoryReleaseService.ValidationAndSort.cs
+++ PowerForge/Services/DotNetRepositoryReleaseService.ValidationAndSort.cs
@@ -1,14 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.Json;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
 
 namespace PowerForge;
 
